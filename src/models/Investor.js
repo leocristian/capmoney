d4ce6819f4927ea35cde51ba29ccb19fd6af0810
leocristian <leonardosclopes@gmail.com>
2021-06-24 16:52:18 -0300

    criando models startup e investor

---
+++ src/models/Investor.js
@@ -0,0 +1,11 @@
+class Investor{
+    constructor(name, email, bio, level){
+        this.name = name
+        this.email = email
+        this.bio = bio
+        this.level = level
+
+    }
+}
+
+module.exports = Investor
---
+++ src/models/StartUp.js
@@ -0,0 +1,9 @@
+class StartUp{
+    constructor(username, passwd) {
+        this.username = username
+        this.passwd = passwd
+        // Colocar o restante das informações de cadastro da StartUp
+    }
+}
+
+module.exports = StartUp
